2e7f58ef48273a88514df57745d1d66ac2d7feda
Nursel Demirkiran <demirkiran3@gmail.com>
2024-01-16 01:19:10 +0300

    login/register pages

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,17 +3,30 @@ import { Link } from "react-router-dom";
 import { Container, Row, Col, Form, FormGroup, Input } from "reactstrap";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/CommonSection";
-
-import "../styles/contact.css";
+import "../styles/register.css";
 
 
 
 const Register = () => {
     return (
         <Helmet title="Register">
-            <CommonSection title="Register" />
-            <section>
+            <section className="register-page-section">
                 <Container>
+                    <div className="register-container">
+                        <div className="register-body">
+                            <div className="register-form">
+                                <input className="form-control" type="text" placeholder="Ad*" />
+                                <input className="form-control" type="text" placeholder="Soyad*" />
+                                <input className="form-control" type="email" placeholder="E-Posta*" />
+                                <input className="form-control" type="password" placeholder="Şifre*" />
+                                <input className="form-control" type="password" placeholder="Şifre Tekrar*" />
+                            </div>
+                        </div>
+                        <div className="register-footer">
+                            <button className="register-button" type="submit">Kayıt Ol</button>
+                            <span>Zaten bir hesabın var mı? <a className="register-footer-link" href="#/login"> Giriş Yap</a></span>
+                        </div>
+                    </div>
 
                 </Container>
             </section>
