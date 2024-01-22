1abc31a8009b8ea21599fe99c37e457c7b45f45a
Nursel Demirkiran <demirkiran3@gmail.com>
2024-01-23 00:56:40 +0300

    updated BookingForm component using FormInput component

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,72 +1,23 @@
 import React from "react";
 import "./booking-form.css";
 import { Form, FormGroup } from "reactstrap";
+import FormInput from "../FormInput/FormInput";
 
 const BookingForm = () => {
   const submitHandler = (event) => {
     event.preventDefault();
   };
+
   return (
     <Form onSubmit={submitHandler}>
-      <FormGroup className="booking__form d-inline-block me-4 mb-4">
-        <input type="text" placeholder="First Name" />
-      </FormGroup>
-      <FormGroup className="booking__form d-inline-block ms-1 mb-4">
-        <input type="text" placeholder="Last Name" />
-      </FormGroup>
-
-      <FormGroup className="booking__form d-inline-block me-4 mb-4">
-        <input type="email" placeholder="Email" />
-      </FormGroup>
-      <FormGroup className="booking__form d-inline-block ms-1 mb-4">
-        <input type="number" placeholder="Phone Number" />
-      </FormGroup>
-
-      <FormGroup className="booking__form d-inline-block me-4 mb-4">
-        <input type="text" placeholder="From Address" />
-      </FormGroup>
-      <FormGroup className="booking__form d-inline-block ms-1 mb-4">
-        <input type="text" placeholder="To Address" />
-      </FormGroup>
-
-      {/* <FormGroup className="booking__form d-inline-block me-4 mb-4">
-        <select name="" id="">
-          <option value="1 person">1 Person</option>
-          <option value="2 person">2 Person</option>
-          <option value="3 person">3 Person</option>
-          <option value="4 person">4 Person</option>
-          <option value="5+ person">5+ Person</option>
-        </select>
-      </FormGroup> */}
-      {/* <FormGroup className="booking__form d-inline-block ms-1 mb-4">
-        <select name="" id="">
-          <option value="1 luggage">1 luggage</option>
-          <option value="2 luggage">2 luggage</option>
-          <option value="3 luggage">3 luggage</option>
-          <option value="4 luggage">4 luggage</option>
-          <option value="5+ luggage">5+ luggage</option>
-        </select>
-      </FormGroup> */}
-
-      <FormGroup className="booking__form d-inline-block me-4 mb-4">
-        <input type="date" placeholder="Journey Date" />
-      </FormGroup>
-      <FormGroup className="booking__form d-inline-block ms-1 mb-4">
-        <input
-          type="time"
-          placeholder="Journey Time"
-          className="time__picker"
-        />
-      </FormGroup>
-
-      <FormGroup>
-        <textarea
-          rows={5}
-          type="textarea"
-          className="textarea"
-          placeholder="Write"
-        ></textarea>
-      </FormGroup>
+      <FormInput name="first-name" placeholder="First Name" formGroupClass="booking__form d-inline-block me-4 mb-4" />
+      <FormInput name="last-name" placeholder="Last Name" formGroupClass="booking__form d-inline-block ms-1 mb-4" />
+      <FormInput type="email" name="email" placeholder="Email" formGroupClass="booking__form d-inline-block me-4 mb-4" />
+      <FormInput name="phone-number" placeholder="Phone Number" formGroupClass="booking__form d-inline-block ms-1 mb-4" />
+      <FormInput name="from-address" placeholder="From Address" formGroupClass="booking__form d-inline-block me-4 mb-4" />
+      <FormInput name="to-address" placeholder="To Address" formGroupClass="booking__form d-inline-block ms-1 mb-4" />
+      <FormInput type="date" name="journey-date" placeholder="Journey Date" formGroupClass="booking__form d-inline-block me-4 mb-4" />
+      <FormInput type="time" name="journey-time" placeholder="Journey Time" formGroupClass="booking__form d-inline-block ms-1 mb-4" inputClass="time__picker" />
     </Form>
   );
 };
