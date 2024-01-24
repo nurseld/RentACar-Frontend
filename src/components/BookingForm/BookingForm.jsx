8e26755a542ba26a2e32515cdb0fd1a6b2d3e532
Nursel Demirkiran <demirkiran3@gmail.com>
2024-01-24 23:34:25 +0300

    updated BookingForm

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -3,7 +3,15 @@ import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import "./booking-form.css";
 import { FormGroup } from "reactstrap";
+import FormInput from "../FormInput/FormInput";
 
+
+// const FormField = ({ type, name, placeholder }) => (
+//     <FormGroup className="booking__form__element d-inline-block  ">
+//         <Field type={type} name={name} placeholder={placeholder} />
+//         <ErrorMessage name={name} component="div" className="error" />
+//     </FormGroup>
+// );
 
 const BookingForm = () => {
     const initialValues = {
@@ -21,7 +29,9 @@ const BookingForm = () => {
         firstName: Yup.string().required("First Name is required"),
         lastName: Yup.string().required("Last Name is required"),
         email: Yup.string().email("Invalid email").required("Email is required"),
-        phoneNumber: Yup.number().typeError("Phone Number must be a number").required("Phone Number is required"),
+        phoneNumber: Yup.string()
+            .typeError("Phone Number must be a number")
+            .required("Phone Number is required"),
         fromAddress: Yup.string().required("From Address is required"),
         toAddress: Yup.string().required("To Address is required"),
         journeyDate: Yup.date().required("Journey Date is required"),
@@ -29,7 +39,7 @@ const BookingForm = () => {
     });
 
     const onSubmit = (values, { resetForm }) => {
-
+        // Handle form submission logic here
         console.log("Form submitted with values:", values);
         resetForm();
     };
@@ -40,51 +50,44 @@ const BookingForm = () => {
             validationSchema={validationSchema}
             onSubmit={onSubmit}
         >
-            <Form>
-                <FormGroup className="booking__form d-inline-block me-4 mb-4">
-                    <Field type="text" name="firstName" placeholder="First Name" />
-                    <ErrorMessage name="firstName" component="div" className="error" />
-                </FormGroup>
-                <FormGroup className="booking__form d-inline-block ms-1 mb-4">
-                    <Field type="text" name="lastName" placeholder="Last Name" />
-                    <ErrorMessage name="lastName" component="div" className="error" />
-                </FormGroup>
-                <FormGroup className="booking__form d-inline-block me-4 mb-4">
-                    <Field type="email" name="email" placeholder="Email" />
-                    <ErrorMessage name="email" component="div" className="error" />
-                </FormGroup>
-                <FormGroup className="booking__form d-inline-block ms-1 mb-4">
-                    <Field
-                        type="text"
-                        name="phoneNumber"
-                        placeholder="Phone Number"
-                    />
-                    <ErrorMessage name="phoneNumber" component="div" className="error" />
-                </FormGroup>
-                <FormGroup className="booking__form d-inline-block me-4 mb-4">
-                    <Field type="text" name="fromAddress" placeholder="From Address" />
-                    <ErrorMessage name="fromAddress" component="div" className="error" />
-                </FormGroup>
-                <FormGroup className="booking__form d-inline-block ms-1 mb-4">
-                    <Field type="text" name="toAddress" placeholder="To Address" />
-                    <ErrorMessage name="toAddress" component="div" className="error" />
-                </FormGroup>
-
-                <FormGroup className="booking__form d-inline-block me-4 mb-4">
-                    <Field type="date" name="journeyDate" placeholder="Journey Date" />
-                    <ErrorMessage name="journeyDate" component="div" className="error" />
-                </FormGroup>
-                <FormGroup className="booking__form d-inline-block ms-1 mb-4">
-                    <Field
-                        type="time"
-                        name="journeyTime"
-                        placeholder="Journey Time"
-                        className="time__picker"
-                    />
-                    <ErrorMessage name="journeyTime" component="div" className="error" />
-                </FormGroup>
-
-                <button type="submit">Submit</button>
+            <Form className="booking__form">
+                {/* <div className="d-flex flex-wrap"> */}
+                {/* Reusable FormField component for each input */}
+                <FormInput
+                    formGroupClass="booking__form__element d-inline-block"
+                    type="text" name="lastName" placeholder="Last Name"
+                />
+                <FormInput
+                    formGroupClass="booking__form__element d-inline-block"
+                    type="text" name="firstName" placeholder="First Name"
+                />
+                <FormInput
+                    formGroupClass="booking__form__element d-inline-block"
+                    type="email" name="email" placeholder="Email"
+                />
+                <FormInput
+                    formGroupClass="booking__form__element d-inline-block"
+                    type="text" name="phoneNumber" placeholder="Phone Number"
+                />
+                <FormInput
+                    formGroupClass="booking__form__element d-inline-block"
+                    type="text" name="fromAddress" placeholder="From Address"
+                />
+                <FormInput
+                    formGroupClass="booking__form__element d-inline-block"
+                    type="text" name="toAddress" placeholder="To Address"
+                />
+                <FormInput
+                    formGroupClass="booking__form__element d-inline-block"
+                    type="date" name="journeyDate" placeholder="Journey Date"
+                />
+                <FormInput
+                    formGroupClass="booking__form__element d-inline-block"
+                    type="time" name="journeyTime" placeholder="Journey Time"
+                    className="time__picker"
+                />
+                {/* </div> */}
+                {/* <button type="submit">Submit</button> */}
             </Form>
         </Formik>
     );
