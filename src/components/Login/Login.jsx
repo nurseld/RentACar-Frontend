3d58b87ab80a0f7b07b255fc812042da7c6a314d
Nursel Demirkiran <demirkiran3@gmail.com>
2024-02-06 19:44:14 +0300

    implemented formik to Login component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from "react-router-dom";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import FormInput from "../FormInput/FormInput";
 import './login.css'
 
 function Login({ title }) {
@@ -14,38 +17,73 @@ function Login({ title }) {
         }
     }, [modalStatus]);
 
+    const initialValues = {
+        email: "",
+        password: "",
+    };
 
+    const validationSchema = Yup.object().shape({
+        email: Yup.string().required("Email is required"),
+        password: Yup.string().required("Password is required"),
+    });
+
+    const onSubmit = async (values, { resetForm }) => {
+        // Handle form submission logic here
+        console.log("Form submitted with values:", values);
+        try {
+            // const response = await axiosInstance.post(`rentals/add`, { ...values, userId: 9, carId: id });
+            // console.log('Response:', response);
+            // navigate("/order-complete", { state: { info: response.data, rental: values } })
+        } catch (error) {
+            console.error('Veri çekme hatası:', error);
+        }
+        resetForm();
+    };
 
     return (
-        <div className="login-container">
-            <div className="login-body">
-                <div className="title">
-                    Hoş Geldiniz,
-                    <br></br>
-                    {title} Hesabınızla Giriş Yapabilirsiniz
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+        >
+            <Form>
+                <div className="login-container">
+                    <div className="login-body">
+                        <div className="title">
+                            Hoş Geldiniz,
+                            <br></br>
+                            {title} Hesabınızla Giriş Yapabilirsiniz
 
+                        </div>
+                        <div className="login-form">
+                            <FormInput
+                                inputClass="form-control"
+                                type="email" name="email" placeholder="E-posta"
+                            />
+                            <FormInput
+                                inputClass="form-control"
+                                type="password" name="password" placeholder="Şifre"
+                            />
+                        </div>
+                    </div>
+                    <div className="login-footer">
+                        <button className="login-button" type="submit" >Giriş Yap</button>
+                        <a href="#">Şifremi Unuttum</a>
+                        <span>Henüz üye değil misin?
+                            <Link
+                                to="/register"
+                                data-bs-toggle="modal"
+                                data-bs-target="#staticBackdrop"
+                                onClick={async () => await setModalStatus(true)}
+                                className="login-footer-link"
+                            >
+                                Kayıt Ol
+                            </Link>
+                        </span>
+                    </div>
                 </div>
-                <div className="login-form">
-                    <input className="form-control" type="email" placeholder="E-Posta" />
-                    <input className="form-control" type="password" placeholder="Şifre" />
-                </div>
-            </div>
-            <div className="login-footer">
-                <button className="login-button" type="submit" >Giriş Yap</button>
-                <a href="#">Şifremi Unuttum</a>
-                <span>Henüz üye değil misin?
-                    <Link
-                        to="/register"
-                        data-bs-toggle="modal"
-                        data-bs-target="#staticBackdrop"
-                        onClick={async () => await setModalStatus(true)}
-                        className="login-footer-link"
-                    >
-                        Kayıt Ol
-                    </Link>
-                </span>
-            </div>
-        </div>
+            </Form>
+        </Formik>
     )
 }
 
