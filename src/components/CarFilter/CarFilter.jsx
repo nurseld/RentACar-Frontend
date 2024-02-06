1d0d3bea62dcc38a55b37e94234b538885f0bfde
Nursel Demirkiran <demirkiran3@gmail.com>
2024-02-06 15:40:45 +0300

    updated CarFilter component

--- src/components/CarFilter/CarFilter.jsx
+++ src/components/CarFilter/CarFilter.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./car-filter.css";
+import axiosInstance from '../../core/utils/interceptors/axiosInterceptors';
 
-function CarFilter() {
+function CarFilter({ name, id, labelClassForColor = "secondary" }) {
 
+    const [carData, setCarData] = useState([]);
+    const [error, setError] = useState(null);
+
+    const fetchData = async () => {
+        try {
+            const response = await axiosInstance.get("brands/getAll");
+            console.log(response);
+            setCarData(response.data);
+        } catch (error) {
+            console.error("Veri çekme hatası:", error);
+            setError("Veri çekme hatası");
+        }
+    };
+
+    useEffect(() => {
+
+        fetchData();
+    }, []);
 
     return (
 
-        <div className='car-filter'>
-            <input type="checkbox" className="btn-check" id="btn-check-2-outlined" autocomplete="off" />
-            <label className="btn btn-outline-secondary" for="btn-check-2-outlined">Checked</label><br />
-        </div>
-
+        <>
+            <input type="checkbox" className="btn-check" id={id} />
+            <label
+                className={`btn btn-outline-${labelClassForColor}`}
+            >{name}
+            </label>
+        </>
     )
 }
 
