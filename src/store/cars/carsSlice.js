2c6c0285b20f558cca947ee1cff8bcf46f60d458
nurseld <demirkiran3@gmail.com>
2024-02-07 19:41:46 +0300

    updated carsSlice and CarList to get the cars from global state

--- src/store/cars/carsSlice.js
+++ src/store/cars/carsSlice.js
@@ -1,20 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axiosInstance from '../../core/utils/interceptors/axiosInterceptors';
+import { useEffect } from "react";
+import carService from "../../services/carService";
 
-const initialState = {
-    initialCars: [],
+const getCars = async () => {
+    try {
+        const responseData = carService.getAll()
+            .then(response => { return response.data })
+            .catch(error => { return [] });
+        return responseData;
+    } catch (error) {
+        console.log("Error fetching cars:", error);
+    }
+
+    // try {
+    //     const response = await axiosInstance.get("cars/getAll");
+    //     return response.data;
+    // } catch (error) {
+    //     console.log("Error fetching cars:", error);
+    // }
 };
 
 const carsSlice = createSlice(
     {
         name: "cars",
-        initialState,
+        initialState: {
+            cars: await getCars(),
+        },
         reducers: {
-            setInitialCars: (state, action) => {
-                return { ...state, initialCars: [...state.initialCars, action.payload] };
+            setCars: (state, action) => {
+                return { ...state, cars: [...state.cars, action.payload] };
             }
         }
     }
 )
 
 export const carsReducer = carsSlice.reducer;
-export const { setInitialCars } = carsSlice.actions;
+export const { setCars } = carsSlice.actions;
