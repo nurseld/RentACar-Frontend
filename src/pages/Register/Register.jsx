64fd458e44b44ddc9999898d41fb33aad0e323b1
Melisa Selin Welfringer <melisaselinmusellim@gmail.com>
2024-02-08 22:52:08 +0300

    Yup validation update

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Container, Row, Col, Form, FormGroup, Input } from "reactstrap";
+import { Container, Row, Col, FormGroup, Input } from "reactstrap";
 import Helmet from "../../components/Helmet/Helmet";
 import CommonSection from "../../components/CommonSection/CommonSection";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import FormInput from "../../components/FormInput/FormInput";
 import "./register.css";
+import authService from "../../services/authService";
 
 
 
@@ -15,8 +19,71 @@ const Register = () => {
         setDateInputType("date");
     };
 
-    const deactivateDateInput = () => {
-        setDateInputType("text");
+    const corporateInitialValues = {
+        companyName: "",
+        taxNo: "",
+        contactName: "",
+        phoneNumber: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    };
+
+    const corporateValidationSchema = Yup.object().shape({
+        companyName: Yup.string().required("Company name is required"),
+        taxNo: Yup.string().required("Tax No is required"),
+        contactName: Yup.string().required("Contact name is required"),
+        phoneNumber: Yup.string().required("Phone number is required"),
+        email: Yup.string().email.required("Email is required"),
+        password: Yup.string().min(6).max(12).required("Password is required"),
+        confirmPassword: Yup.string().min(6).max(12).required("Password confirmation doesnt match. Please try again.").oneOf[Yup.ref("password")]
+    });
+
+
+    const corporateOnSubmit = async (values, { resetForm }) => {
+        // Handle form submission logic here
+        console.log("Form submitted with values:", values);
+        try {
+            // const response = await axiosInstance.post(`rentals/add`, { ...values, userId: 9, carId: id });
+            // console.log('Response:', response);
+            // navigate("/order-complete", { state: { info: response.data, rental: values } })
+        } catch (error) {
+            console.error('Veri çekme hatası:', error);
+        }
+        resetForm();
+    };
+
+    const individualInitialValues = {
+        firstName: "",
+        lastName: "",
+        nationalIdNo: "",
+        birthDate: "",
+        phoneNumber: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    };
+
+    const individualValidationSchema = Yup.object().shape({
+        firstName: Yup.string().required("First name is required"),
+        lastName: Yup.string().required("Last name is required"),
+        nationalIdNo: Yup.string().required("TC No is required"),
+        birthDate: Yup.string().required("Birth date is required"),
+        phoneNumber: Yup.string().required("Phone number is required"),
+        email: Yup.string().email.required("Email is required"),
+        password: Yup.string().min(6).max(12).required("Password is required"),
+        confirmPassword: Yup.string().min(6).max(12).required("Password confirmation doesnt match. Please try again.").oneOf[Yup.ref("password")]
+    });
+
+    const individualOnSubmit = async (values, { resetForm }) => {
+        // Handle form submission logic here
+        console.log("Form submitted with values:", values);
+        try {
+            authService.customerRegister(values)
+        } catch (error) {
+            console.error('Veri çekme hatası:', error);
+        }
+        resetForm();
     };
 
 
@@ -25,8 +92,6 @@ const Register = () => {
         <Helmet title="Register">
             <section className="register-page-section">
                 <Container>
-
-
                     <div className="register-container">
                         <ul className="nav nav-tabs" id="myTab" role="tablist">
                             <li className="nav-item" role="presentation">
@@ -39,50 +104,64 @@ const Register = () => {
                         <div className="tab-content" id="myTabContent">
                             <div className="tab-pane fade show active" id="register-individual" role="tabpanel" aria-labelledby="register-individual-tab">
 
-                                <div className="individual-register-body">
-                                    <div className="register-form">
-                                        <input className="form-control" type="text" placeholder="Ad*" />
-                                        <input className="form-control" type="text" placeholder="Soyad*" />
-                                        <input className="form-control" type="text" placeholder="TC No*" />
-                                        <input
-                                            className="form-control"
-                                            type={dateInputType}
-                                            placeholder="Doğum Tarihi"
-                                            id="date"
-                                            onFocus={activateDateInput}
-                                            onBlur={deactivateDateInput}
-                                            pattern="\d{2}\d{2}\d{4}"
-                                        />
-                                        <input className="form-control" type="text" placeholder="Telefon*" />
-                                        <input className="form-control" type="email" placeholder="E-Posta*" />
-                                        <input className="form-control" type="password" placeholder="Şifre*" />
-                                        <input className="form-control" type="password" placeholder="Şifre Tekrar*" />
-                                    </div>
-                                </div>
-                                <div className="register-footer">
-                                    <button className="register-button" type="submit">Kayıt Ol</button>
-                                </div>
+                                <Formik
+                                    initialValues={individualInitialValues}
+                                    validationSchema={individualValidationSchema}
+                                    onSubmit={individualOnSubmit}
+                                >
+                                    <Form>
+                                        <div className="individual-register-body">
+                                            <div className="register-form page-form">
+                                                <FormInput formGroupClass="m-top-20" inputClass="form-control" name="firstName" placeholder="Ad*" />
+                                                <FormInput formGroupClass="m-top-20" inputClass="form-control" name="lastName" placeholder="Soyad*" />
+                                                <FormInput inputClass="form-control" name="nationalIdNo" placeholder="TC No*" />
+                                                <FormInput
+                                                    inputClass="form-control"
+                                                    type={dateInputType}
+                                                    name="birthDate"
+                                                    id="date"
+                                                    placeholder="Doğum Tarihi*"
+                                                    onFocus={activateDateInput}
+                                                    pattern="\d{2}\d{2}\d{4}"
+                                                />
+                                                <FormInput inputClass="form-control" name="phoneNumber" placeholder="Telefon*" />
+                                                <FormInput inputClass="form-control" type="email" name="email" placeholder="E-Posta*" />
+                                                <FormInput inputClass="form-control" type="password" name="password" placeholder="Şifre*" />
+                                                <FormInput inputClass="form-control" type="password" name="confirmPassword" placeholder="Şifre Tekrar*" />
+                                            </div>
+                                        </div>
+                                        <div className="register-footer">
+                                            <button className="register-button" type="submit">Kayıt Ol</button>
+                                        </div>
+                                    </Form>
+                                </Formik>
 
 
 
                             </div>
                             <div className="tab-pane fade" id="register-corporate" role="tabpanel" aria-labelledby="register-corporate-tab">
-                                <div className="corporate-register-body">
-                                    <div className="register-form">
-                                        <input className="form-control" type="text" placeholder="Yetkili Ad*" />
-                                        <input className="form-control" type="text" placeholder="Yetkili Soyad*" />
-                                        <input className="form-control" type="text" placeholder="Company Name" />
-                                        <input className="form-control" type="text" placeholder="Tax No" />
-                                        <input className="form-control" type="text" placeholder="Telefon*" />
-                                        <input className="form-control" type="email" placeholder="E-Posta*" />
-                                        <input className="form-control" type="password" placeholder="Şifre*" />
-                                        <input className="form-control" type="password" placeholder="Şifre Tekrar*" />
-                                    </div>
-                                </div>
-                                <div className="register-footer">
-                                    <button className="register-button" type="submit">Kayıt Ol</button>
-
-                                </div>
+                                <Formik
+                                    initialValues={corporateInitialValues}
+                                    validationSchema={corporateValidationSchema}
+                                    onSubmit={corporateOnSubmit}
+                                >
+                                    <Form>
+                                        <div className="corporate-register-body">
+                                            <div className="register-form page-form">
+                                                <FormInput formGroupClass="m-top-20 width-100" inputClass="form-control full-size" name="companyName" placeholder="Company Name*" />
+                                                <FormInput inputClass="form-control" name="taxNo" placeholder="Tax No*" />
+                                                <FormInput inputClass="form-control" name="contactName" placeholder="Yetkili Ad Soyad*" />
+                                                <FormInput inputClass="form-control" name="phoneNumber" placeholder="Telefon*" />
+                                                <FormInput inputClass="form-control" name="email" placeholder="E-Posta*" />
+                                                <FormInput inputClass="form-control" name="password" placeholder="Şifre*" />
+                                                <FormInput inputClass="form-control" name="confirmPassword" placeholder="Şifre Tekrar*" />
+                                            </div>
+                                        </div>
+                                        <div className="register-footer">
+                                            <button className="register-button" type="submit">Kayıt Ol</button>
+                                        </div>
+                                    </Form>
+                                </Formik>
                             </div>
                         </div>
                     </div>
