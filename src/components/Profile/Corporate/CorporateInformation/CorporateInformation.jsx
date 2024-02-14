5bf5c86fa865f5a001c26024c887b9faf13b44db
Nursel Demirkiran <demirkiran3@gmail.com>
2024-02-14 12:21:37 +0300

    added CorporateInformation component

---
+++ src/components/Profile/Corporate/CorporateInformation/CorporateInformation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './corporate-information.css'
+
+function CorporateInformation({ corporate }) {
+    return (
+        <></>
+        // <div>{corporate.companyName}</div>
+    )
+}
+
+export default CorporateInformation
