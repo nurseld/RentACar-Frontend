005f568b1dba3a8ab900bf9bf2bfe04e17c983c7
Nursel Demirkiran <demirkiran3@gmail.com>
2024-02-14 12:24:25 +0300

    updated OrderComplete component

--- src/pages/OrderComplete/OrderComplete.jsx
+++ src/pages/OrderComplete/OrderComplete.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-
+import './order-complete.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function OrderComplete() {
     const location = useLocation();
@@ -13,20 +14,21 @@ function OrderComplete() {
     }, []);
 
     return (
-        <div className="container mt-5">
+        <div className="order-complete-container container mb-5 mt-5 p-4">
             <h2 className="text-center mb-4">Order Complete</h2>
 
-            <div className="card mb-4">
-                <div className="card-body">
+            <div className="transaction-information-card card mb-4">
+                <div className="transaction-information-card-body card-body">
                     <h3 className="card-title">Transaction Information:</h3>
                     <p className="card-text">Total Price: {info.totalPrice}</p>
                     <p className="card-text">User ID: {info.userId}</p>
-                    <p className="card-text">Car ID: {info.carId}</p>
+                    <p className="card-text">Car Plate: {info.car.plate}</p>
+                    <img src={info.car.imagePath} />
                 </div>
             </div>
 
-            <div className="card">
-                <div className="card-body">
+            <div className="rental-details-card card mb-5">
+                <div className="rental-details-card-body card-body">
                     <h3 className="card-title">Rental Details:</h3>
                     <p className="card-text">Pick-Up Location: {rental.pickUpLocation}</p>
                     <p className="card-text">Drop-Off Location: {rental.dropOffLocation}</p>
